2874fd6aac5e8cba505f12b4ce9885fb036a1d92
lolaodelola <damzcodes@gmail.com>
2020-10-10 20:50:47 +0100

    ammend typo

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_10_10_193033) do
   enable_extension "plpgsql"
 
   create_table "affirmations", force: :cascade do |t|
-    t.string "affirmation"
+    t.string "affirmations"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
