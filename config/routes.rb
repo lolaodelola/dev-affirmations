26d8fc26ac0de8e9c5070897804fb91e93277e60
lolaodelola <damzcodes@gmail.com>
2020-10-12 23:10:12 +0100

    rm unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'affirmations#new'
   resources :affirmations
-  resources :developers, param: :uuid, defaults: { format: 'xml' } do
-    patch '/confirm', to: 'developers#confirm'
-    get '/confirm', to: 'developers#preconfirm', as: 'preconfirm'
-  end
+  patch 'developers/:developer_uuid/confirm', to: 'developers#confirm', defaults: {format: "xml"}
+  get 'developers/:developer_uuid/confirm', to: 'developers#preconfirm', as: 'preconfirm', defaults: {format: "xml"}
   post '/twilio/receive', to: 'developers#receive'
 end
