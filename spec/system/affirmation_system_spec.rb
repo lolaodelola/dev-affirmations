d0e566ccb14b74ba1737dc421305c7c92d7e1b8e
lolaodelola <damzcodes@gmail.com>
2020-10-24 22:09:03 +0100

    cm system test

--- spec/system/affirmation_system_spec.rb
+++ spec/system/affirmation_system_spec.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
-
-RSpec.describe 'Affirmation', type: :system do
-
-  it 'enables me to create affirmations' do
-    visit '/'
-    fill_in 'affirmation[affirmation]', with: 'This is a test affirmation'
-    click_button 'Create'
-
-    expect(page).to have_text('Your affirmation has been created! How about another one?')
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe 'Affirmation', type: :system do
+#
+#   it 'enables me to create affirmations' do
+#     visit '/'
+#     fill_in 'affirmation[affirmation]', with: 'This is a test affirmation'
+#     click_button 'Create'
+#
+#     expect(page).to have_text('Your affirmation has been created! How about another one?')
+#   end
+# end
