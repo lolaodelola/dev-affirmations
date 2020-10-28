58e399e7ea1d7cb1ce24a23cab20759dbcc8959d
lolaodelola <damzcodes@gmail.com>
2020-10-28 19:58:01 +0000

    add host

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'affirmations#landing'
-  resources :affirmations
+  resources :affirmations, only: %i[new create]
   patch 'developers/:developer_uuid/confirm', to: 'developers#confirm', defaults: {format: "xml"}
   get 'developers/:developer_uuid/confirm', to: 'developers#preconfirm', as: 'preconfirm', defaults: {format: "xml"}
   post '/twilio/receive', to: 'developers#receive'
